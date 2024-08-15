97974c192cdf41d537b4acf12191f2802bfd7baa
unknown <yazdanih80.ce@gmail.com>
2024-08-15 09:31:34 +0330

    fifth js practice (power **)

---
+++ FifthP.js
@@ -0,0 +1,12 @@
+// var userNum1 = +prompt("لطفا عدد اول را وارد کنید");
+// while (isNaN(userNum1)) {
+//   alert("استفاده از حروف مجاز نمیباشد");
+//   var userNum1 = +prompt("لطفا عدد اول را مجدد وارد کنید");
+// }
+// var userNum2 = +prompt("لطفا عدد دوم را وارد کنید");
+// while (isNaN(userNum2)) {
+//   alert("استفاده از حروف مجاز نمیباشد");
+//   var userNum2 = +prompt("لطفا عدد دوم را مجدد وارد کنید");}
+
+// var pow = userNum1 ** userNum2;
+// console.log("result pow : " + pow);
