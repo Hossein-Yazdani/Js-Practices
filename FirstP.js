91dbbb40452188424441acad3be0c8400a94f387
unknown <yazdanih80.ce@gmail.com>
2024-08-15 08:46:34 +0330

    coding second js practice(isEve = num)

--- FirstP.js
+++ FirstP.js
@@ -1,12 +1,23 @@
-var userNum1 = +prompt("لطفا عدد اول را وارد کنید");
-var userNum2 = +prompt("لطفا عدد دوم را وارد کنید");
-var sum = userNum1 + userNum2;
-console.log("result sum : " + sum);
-var sub = userNum1 - userNum2;
-console.log("result sub : " + sub);
-var mul = userNum1 * userNum2;
-console.log("result mul : " + mul);
-var div = userNum1 / userNum2;
-console.log("result div : " + div);
-var pow = userNum1 ** userNum2;
-console.log("result pow : " + pow);
+// var userNum1 = +prompt("لطفا عدد اول را وارد کنید");
+// while (isNaN(userNum1)) {
+    
+//     alert("استفاده از حروف مجاز نمیباشد");
+//     var userNum1 = +prompt("لطفا عدد اول را دوباره وارد کنید");
+// }
+// var userNum2 = +prompt("لطفا عدد دوم را دوباره وارد کنید");
+// while(isNaN(userNum2)){
+    
+//     alert("استفاده از حروف مجاز نمیباشد");
+//     var userNum2 = +prompt("لطفا عدد دوم را وارد کنید");
+// }
+
+// var sum = userNum1 + userNum2;
+// console.log("result sum : " + sum);
+// var sub = userNum1 - userNum2;
+// console.log("result sub : " + sub);
+// var mul = userNum1 * userNum2;
+// console.log("result mul : " + mul);
+// var div = userNum1 / userNum2;
+// console.log("result div : " + div);
+// var pow = userNum1 ** userNum2;
+// console.log("result pow : " + pow);
