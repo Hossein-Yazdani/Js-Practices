9b57d75bf03e94d8b66f7af03fe4b2331a4a8488
unknown <yazdanih80.ce@gmail.com>
2024-08-15 09:50:36 +0330

    sixth js practice (about age from birth date)

---
+++ sixthP.js
@@ -0,0 +1,17 @@
+var userDate = +prompt("لطفا تاریخ تولد خود را وارد کنید");
+while (isNaN(userDate)) {
+  alert("فقط استفاده از ارقام مجاز میباشد لطفا دوباره امتحان کنید!");
+  var userDate = +prompt("لطفا تاریخ تولد خود را دوباره وارد کنید");
+}
+var userAge = null;
+if (userDate <= 1403) {
+  userAge = 1403 - userDate;
+  console.log("your age : " + userAge);
+
+}else if (userDate > 1900) {
+    userAge = 2024 - userDate;
+    console.log("your age : " + userAge);
+}
+ else {
+  alert("بر اساس محاسبات من شما هنوز به دنیا نیامده اید!!!");
+}
