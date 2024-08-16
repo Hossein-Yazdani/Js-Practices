c5be24d099c8a5a0e0ce2c1e8239263079182470
unknown <yazdanih80.ce@gmail.com>
2024-08-16 12:56:19 +0330

    calculateing avg with function

--- TenthP.js
+++ TenthP.js
@@ -1,13 +1,13 @@
-var userNum = +prompt("لطفا عددی را وارد کنید");
-function isEven() {
-  while (isNaN(userNum)) {
-    alert("شما مجاز به وارد کردن رتشه یا علامت نیستید لطفا مجدد تلاش کنید!");
-    userNum = +prompt("لطفا عددی را وارد کنید");
-  }
-  if (userNum % 2 === 0) {
-    console.log(userNum + " : " + "عدد زوج است");
-  } else {
-    console.log(userNum + " : " + "عدد فرد میباشد");
-  }
-}
-isEven();
+// var userNum = +prompt("لطفا عددی را وارد کنید");
+// function isEven() {
+//   while (isNaN(userNum)) {
+//     alert("شما مجاز به وارد کردن رتشه یا علامت نیستید لطفا مجدد تلاش کنید!");
+//     userNum = +prompt("لطفا عددی را وارد کنید");
+//   }
+//   if (userNum % 2 === 0) {
+//     console.log(userNum + " : " + "عدد زوج است");
+//   } else {
+//     console.log(userNum + " : " + "عدد فرد میباشد");
+//   }
+// }
+// isEven();
