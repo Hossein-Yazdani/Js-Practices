7f403568d6d51380a93506e9e03a1643bd619318
unknown <yazdanih80.ce@gmail.com>
2024-08-16 12:30:36 +0330

    switch case practice

---
+++ NinthP.js
@@ -0,0 +1,31 @@
+// var userAverage = +prompt("لطفا معدل را وارد کنید");
+// if (isNaN(userAverage)) {
+//   while (isNaN(userAverage)) {
+//     alert("فقط استفاده از ارقام مجاز میباشد لطفا دوباره امتحان کنید!");
+//     var userAverage = +prompt("لطفا معدل را دوباره وارد کنید");
+//   }}else{
+
+//       switch (userAverage) {
+//         case 18:
+//         case 19:
+//         case 20:
+//           alert("شما جز دسته اول بوده و سطح شما A میباشد");
+    
+//           break;
+//         case 15:
+//         case 16:
+//         case 17:
+//           alert("شما جز دسته دوم بوده و سطح شما B میباشد");
+    
+//           break;
+//         case 12:
+//         case 13:
+//         case 14:
+//           alert("شما جز دسته سوم بوده و سطح شما C میباشد");
+//           break;
+
+//           default:
+//             alert("شما مشروط شدید");
+//       }
+//     }
+
