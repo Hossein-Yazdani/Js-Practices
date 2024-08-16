6d59c944a8737b8a3a645c1c994b3051dac384fa
unknown <yazdanih80.ce@gmail.com>
2024-08-16 17:56:18 +0330

    sum userproduct prices with for

---
+++ SixteenthP.js
@@ -0,0 +1,10 @@
+// var userProductPrice = null;
+// for(i=0;i<5;i++){
+    
+//     while(isNaN(userProductPrice)){
+//         alert("فقط استفاده از ارقام مجاز میباشد لطفا دوباره امتحان کنید!");
+//         userProductPrice = +prompt("لطفا قیمت محصول را وارد کنید");
+//     }
+//     userProductPrice=userProductPrice + +prompt("لطفا قیمت محصول را وارد کنید");
+// }
+// console.log("قیمت کل محصولات : " + userProductPrice)
