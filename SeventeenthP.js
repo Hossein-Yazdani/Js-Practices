42bdeae7c2c993b45780afdd354130eb9951bd75
unknown <yazdanih80.ce@gmail.com>
2024-08-16 18:32:48 +0330

    using <for> for getting user num and calculateing avg

---
+++ SeventeenthP.js
@@ -0,0 +1,13 @@
+// var userNum = null;
+// for (i = 0; i < 5; i++) {
+//   while (isNaN(userNum)) {
+//     alert("فقط استفاده از ارقام مجاز میباشد لطفا دوباره امتحان کنید!");
+//     userNum = +prompt("لطفا عددی را وارد کنید");
+//     i = 1;
+//   }
+//   userNum = userNum + +prompt("لطفا عددی را وارد کنید");
+//   if (isNaN(userNum)) {
+//     i = 0;
+//   }
+// }
+// console.log("sum : " + userNum + "\n\n" + "avg : " + userNum / 5);
--- SixteenthP.js
+++ SixteenthP.js
@@ -4,7 +4,10 @@
 //     while(isNaN(userProductPrice)){
 //         alert("فقط استفاده از ارقام مجاز میباشد لطفا دوباره امتحان کنید!");
 //         userProductPrice = +prompt("لطفا قیمت محصول را وارد کنید");
+//         i = 1;
 //     }
 //     userProductPrice=userProductPrice + +prompt("لطفا قیمت محصول را وارد کنید");
+//     if(isNaN(userProductPrice)){
+//         i=0}
 // }
 // console.log("قیمت کل محصولات : " + userProductPrice)
