d6829ebf02d353136a1a15e4dfb7a5c610217c21
unknown <yazdanih80.ce@gmail.com>
2024-08-16 09:32:10 +0330

    new practice js(converting minutes to hour

---
+++ Seventh.js
@@ -0,0 +1,11 @@
+var userMinutes = +prompt("لطفا زمان را به دقیقه وارد کنید!")
+var hour = null
+    if(isNaN(userMinutes)){
+        while(isNaN(userMinutes)){
+            alert("زمان فقط مقدار عددی میگیرد دوباره امتحان کنید!")
+            userMinutes = +prompt("لطفا زمان را به دقیقه وارد کنید!")}
+            hour = userMinutes/60
+    }else{
+        hour = userMinutes/60
+    }
+console.log(hour+" "+"hour")
