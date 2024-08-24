78dd360c12575823660d3faa583ce0585d0a1a76
unknown <yazdanih80.ce@gmail.com>
2024-08-24 17:23:54 +0330

    new practice using array and object for registeration

--- Practice18.js
+++ Practice18.js
@@ -1,25 +1,25 @@
-var userNumCount 
-userNumCount = +prompt("چند عدد مدنظر دارید برای وارد کردن؟");
-while (isNaN(userNumCount)) {
-    alert("فقط استفاده از ارقام مجاز میباشد لطفا از اول امتحان کنید!");
-    userNumCount = +prompt("چند عدد مدنظر دارید برای وارد کردن؟");
-}
-var userNum;
-var userNumArray = [];
-while (userNumCount > 0) {
-    userNum = +prompt("لطفا عدد " + userNumCount + " را وارد کنید");
-    if(isNaN(userNum)){
-       while(isNaN(userNum)){
-        alert("فقط استفاده از ارقام مجاز میباشد لطفا از اول امتحان کنید!");
-        userNum = +prompt("لطفا عدد " + userNumCount + " را وارد کنید");
-       } 
-    }
-  userNumCount--;
-  userNumArray.push(userNum);
-}
-var sum = null
-userNumArray.forEach(function (userNumSum) {
-    sum += userNumSum
+// var userNumCount 
+// userNumCount = +prompt("چند عدد مدنظر دارید برای وارد کردن؟");
+// while (isNaN(userNumCount)) {
+//     alert("فقط استفاده از ارقام مجاز میباشد لطفا از اول امتحان کنید!");
+//     userNumCount = +prompt("چند عدد مدنظر دارید برای وارد کردن؟");
+// }
+// var userNum;
+// var userNumArray = [];
+// while (userNumCount > 0) {
+//     userNum = +prompt("لطفا عدد " + userNumCount + " را وارد کنید");
+//     if(isNaN(userNum)){
+//        while(isNaN(userNum)){
+//         alert("فقط استفاده از ارقام مجاز میباشد لطفا از اول امتحان کنید!");
+//         userNum = +prompt("لطفا عدد " + userNumCount + " را وارد کنید");
+//        } 
+//     }
+//   userNumCount--;
+//   userNumArray.push(userNum);
+// }
+// var sum = null
+// userNumArray.forEach(function (userNumSum) {
+//     sum += userNumSum
 
-})
-console.log("sum : " + sum + "\n" + "avg : "+ sum / userNumArray.length);
+// })
+// console.log("sum : " + sum + "\n" + "avg : "+ sum / userNumArray.length);
---
+++ Practice19.js
@@ -0,0 +1,29 @@
+// var userInfo = [
+//   { id: 1, name: "Hossein", age: 25, address: "Tehran" },
+//   { id: 2, name: "Alireza", age: 20, address: "Gheshm" },
+//   { id: 3, name: "Reza", age: 30, address: "Kish" },
+// ];
+// var userRegisterName = prompt("لطفا نام خود را وارد کنید");
+// while (userRegisterName.length > 6 || userRegisterName.length < 3) {
+//   alert("مقدار وارد شده صحیح نیست لطفا مجدد تلاش کنید!!");
+//   userRegisterName = prompt("لطفا نام خود را وارد کنید");
+// }
+// var userRegisterAge = +prompt("لطفا سن خود را وارد کنید");
+// while (isNaN(userRegisterAge)) {
+//   alert("مقدار وارد شده صحیح نیست لطفا مجدد تلاش کنید!!");
+//   userRegisterAge = prompt("لطفا سن خود را وارد کنید");
+// }
+// var userRegisterAddress = prompt("لطفا آدرس خود را وارد کنید");
+// while (userRegisterAddress.length > 10 || userRegisterAddress.length < 3) {
+//   alert("مقدار وارد شده صحیح نیست لطفا مجدد تلاش کنید!!");
+//   userRegisterAddress = prompt("لطفا آدرس خود را وارد کنید");
+// }
+// id = 4;
+// var userRegister = {
+//   id: id,
+//   name: userRegisterName,
+//   age: userRegisterAge,
+//   address: userRegisterAddress,
+// };
+// userInfo.push(userRegister);
+// console.log(userInfo);
